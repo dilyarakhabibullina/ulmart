195e085ae2802b3ced15f60b949649d6d7112d4d
dilyarakhabibullina <khdilyara@rambler.ru>
2019-11-09 00:37:42 -0500

    все методы работают, осталось реализовать поиск по рейтингу и сделать компараторы лямбдами

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,5 +1,6 @@
 package ru.itpark;
 
+import ru.itpark.comparator.DescComparator;
 import ru.itpark.domain.Dress;
 import ru.itpark.domain.Oven;
 import ru.itpark.domain.Product;
@@ -18,16 +19,18 @@ public class Main {
         Repository repository = new Repository();
         service.add(new Product(0, "someproduct", "unknown", 3_000, true));
         service.add(new Dress(0, "dress Dior", "greate", 190_000, true, 4, "blue"));
-        service.add(new Oven(0, "panasonic micriwave oven", "awful", 5_000, false, 4, 600));
-        service.add(new Oven(0, "sumsung micriwave oven", "not bad", 7_000, true, 400, 800));
+        service.add(new Oven(0, "panasonic microwave oven", "awful", 5_000, false, 4, 600));
+        service.add(new Oven(0, " a samsung microwave oven", "not bad", 7_000, true, 400, 800));
         service.add(new Dress(0, "longsleeve dress", "nice", 4_000, true, 1, "green"));
         service.add(new Dress(0, "dress", "nice", 5_500, true, 2, "red"));
 
         System.out.println(service);
-        System.out.println(service.removeById());
-        System.out.println(service.searchByPrice());
+        System.out.println(service.searchByCategory());
+        System.out.println(service.removeById(4));
+        System.out.println(service.searchByPrice(4000, 5500));
+        System.out.println(service.searchByName("micro"));
         System.out.println(service.getSortedByPrice());
-        //System.out.println(service.getSortedBy(new HouseByPriceDescComparator()));
+        System.out.println(service.getSortedBy(new DescComparator()));
         System.out.println(service.getSortedBy(new Comparator<Product>() {
             @Override
             public int compare(Product o1, Product o2) {
--- src/main/java/ru/itpark/service/ProductService.java
+++ src/main/java/ru/itpark/service/ProductService.java
@@ -10,12 +10,11 @@ import ru.itpark.repository.Repository;
 import java.util.*;
 
 public class ProductService implements Comparator {
-    private final Repository repository;
 
+    private final Repository repository;
     public ProductService(Repository repository) {
         this.repository = repository;
     }
-
     public void add(Product product) {
         if (product.getId() != 0) {
             throw new IllegalArgumentException("id must be zero!");
@@ -26,10 +25,12 @@ public class ProductService implements Comparator {
         repository.addProduct(product);
     }
 
+    //TODO- сделать сервис addAll и использовать его в main
     private Collection<Product> product = new ArrayList<>();
 
     public List<Product> getSortedByPrice() {
         return getSortedBy(new AscComparator());
+        //TODO- use lambda
     }
 
     public List<Product> getSortedBy(Comparator<Product> comparator) {
@@ -39,63 +40,79 @@ public class ProductService implements Comparator {
     }
 
     public List<Product> searchByName(String text) {
-        List<Product> products = new ArrayList<>();
-        for (Product product1 : products) {
+        List<Product> product = new ArrayList<>();
+        for (Product product1 : repository.getAllProducts()) {
             if (product1.getName().contains(text)) {
-                products.add(product1);
-            }
-        }
-        return products;
-    }
-
-    public List<Product> searchByPrice() {
-        //  int min = 4000; int max = 100_000;
-        List<Product> resultPrice = new ArrayList<>();
-        //почему при попытке передать все продукты в список resultPrice (как написано на строчке ниже) при запуске
-        // вылезает exception??? Как избежать этого исключения?
-        // List<Product> resultPrice = new ArrayList<>(repository.getAllProducts());
-
-        for (Product product : resultPrice) {
-
-            if (product.getPrice() > 4000 && product.getPrice() <= 5500) {
-                resultPrice.add(product);
-                resultPrice.sort(new Comparator<Product>() {
+                product.add(product1);
+                product.sort(new Comparator<Product>() {
                     @Override
                     public int compare(Product o1, Product o2) {
-                        return o1.getPrice() - o2.getPrice();
+                        return o1.getName().compareToIgnoreCase(o2.getName());
                     }
                 });
             }
         }
-
-
-        return resultPrice;
-    }
-
-
-    public List<Product> removeById() {
-        ArrayList<Product> product = new ArrayList<>();
-        product.add(new Product(1, "someproduct", "unknown", 3_000, true));
-        product.add(new Dress(2, "dress Dior", "greate", 190_000, true, 4, "blue"));
-        product.add(new Oven(3, "panasonic micriwave oven", "awful", 5_000, false, 4, 600));
-        product.removeIf(o -> o.getId() == 1);
         return product;
     }
 
+    public List<Product> searchByPrice(int min, int max) {
+        List<Product> resultByPrice = new ArrayList<>();
+        for (Product house : repository.getAllProducts()) {
+            if (min < house.getPrice() && max >= house.getPrice()) {
+                resultByPrice.add(house);
+                resultByPrice.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return  o1.getName().compareToIgnoreCase(o2.getName());
+                    }
+                });
+            }
+        }
+        return resultByPrice;
+    }
+
+    public List<Product> removeById(int id) {
+        ArrayList<Product> product = new ArrayList<>(repository.getAllProducts());
+//        product.add(new Product(1, "someproduct", "unknown", 3_000, true));
+//        product.add(new Dress(2, "dress Dior", "greate", 190_000, true, 4, "blue"));
+//        product.add(new Oven(3, "panasonic micriwave oven", "awful", 5_000, false, 4, 600));
+        product.removeIf(o -> o.getId() == id);
+        return product;
+    }
+
+
+    public List<Product> searchByCategory() {
+        List<Product> listByCategory = new ArrayList<>();
+        for (Product product1 : repository.getAllProducts()) {
+            if (product1 instanceof Oven) {
+                listByCategory.add(product1);
+                listByCategory.sort(new Comparator<Product>() {
+                    @Override
+                    public int compare(Product o1, Product o2) {
+                        return o1.getName().compareToIgnoreCase(o2.getName());
+                    }
+                });
+            }
+        }
+        return listByCategory;
+    }
 
     @Override
     public int compare(Object o1, Object o2) {
         return 0;
     }
-    @Override
-    public String toString() {
-        return "ProductService{" +
-                "repository=" + repository +
-                ", product=" + product +
-                '}';
-    }
 
 }
+
+//            @Override
+//            public String toString () {
+//                return "ProductService{" +
+//                        "repository=" + repository +
+//                        ", product=" + product +
+//                        '}';
+//            }
+//
+//}
 
 //    public List<Product> searchByName() {
 //        ArrayList<Product> result = new ArrayList<>();
